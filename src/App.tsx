11a80b1da555b606eefe54f732d620e279fd0f49
Melvin Young <melviny6@gmail.com>
2020-04-29 09:34:42 -0500

    initial file setup, included reactstrap and styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+// import './App.scss';
+import { Container, Row, Col } from 'reactstrap';
+import styled from 'styled-components';
 
+// const style = {
+// 	backgroundColor: 'grey',
+// 	height: '1000px',
+// };
+
+const StyledCol = styled(Col)`
+	&&& {
+		background-color: grey;
+		height: 5rem;
+		border: 0.1rem solid white;
+	}
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Fragment>
+			<Container fluid className="py-5">
+				<Row>
+					<StyledCol lg={4}>Hello</StyledCol>
+					<StyledCol lg={4}>Hello</StyledCol>
+					<StyledCol lg={4}>Hello</StyledCol>
+				</Row>
+			</Container>
+			<Container fluid className="py-5">
+				<Row>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+					<StyledCol sm={6} md={4} lg={2} xl={1}></StyledCol>
+				</Row>
+			</Container>
+		</Fragment>
+	);
 }
 
 export default App;
