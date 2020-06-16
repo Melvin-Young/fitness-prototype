803f4356b2f832d1f27942a90b7c466a1ff54f10
Melvin Young <melviny6@gmail.com>
2020-06-15 20:07:19 -0500

    Make Background Image component modular

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -1,55 +1,61 @@
-import * as React from 'react';
-import styled from 'styled-components';
+import * as React from "react";
+import styled from "styled-components";
 
-const StyledImage = styled('div')<IImageProps>`
-	&.landing {
-		position: relative;
-		width: 100%;
-		height: ${(props) => props.height};
-		display: table;
-		z-index: -1;
+const StyledImage = styled("div")<IImageProps>`
+  &.landing {
+    position: relative;
+    width: 100%;
+    height: ${(props) => props.height};
+    display: table;
+    z-index: -1;
 
-		& > .home-wrap {
-			clip: rect(0 auto auto 0);
-			position: absolute;
-			width: inherit;
-			height: 100%;
-			top: 0;
-			left: 0;
+    & > .home-wrap {
+      clip: rect(0 auto auto 0);
+      position: absolute;
+      width: inherit;
+      height: 100%;
+      top: 0;
+      left: 0;
 
-			& > .home-inner {
-				background-image: url(${(props) => props.imageUrl});
-				position: fixed;
-				height: 100%;
-				width: 100%;
-				background-size: cover;
-				background-position: center;
-				-webkit-transform: translateZ(0);
-				transform: translateZ(0);
-				will-change: transform;
-				display: table;
-			}
-		}
-	}
+      & > .home-inner {
+        background-image: url(${(props) => props.imageUrl});
+        position: fixed;
+        height: 100%;
+        width: 100%;
+        background-size: cover;
+        background-position: center;
+        -webkit-transform: translateZ(0);
+        transform: translateZ(0);
+        will-change: transform;
+        display: table;
+      }
+      & > .content {
+        position: relative;
+        height: ${(props) => props.height};
+      }
+    }
+  }
 `;
 const BackgroundImage: React.FunctionComponent<IImageProps> = (props) => {
-	return (
-		<StyledImage
-			id={props.id}
-			height={props.height}
-			imageUrl={props.imageUrl}
-			className="landing">
-			<div className="home-wrap">
-				<div className="home-inner"></div>
-			</div>
-		</StyledImage>
-	);
+  return (
+    <StyledImage
+      id={props.id}
+      height={props.height}
+      imageUrl={props.imageUrl}
+      className="landing"
+    >
+      <div className="home-wrap">
+        <div className="home-inner"></div>
+      </div>
+      <div className="content">{props.children}</div>
+    </StyledImage>
+  );
 };
 
 export interface IImageProps extends React.HtmlHTMLAttributes<any> {
-	id?: string;
-	height: string;
-	imageUrl: string;
+  id?: string;
+  height: string;
+  imageUrl: string;
 }
 
 export default BackgroundImage;
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,27 +1,27 @@
-import * as React from 'react';
-import styled from 'styled-components';
+import * as React from "react";
+import styled from "styled-components";
 
 const StyledSection = styled.section`
-	height: 100vh;
-	background-color: transparent;
+  height: 100vh;
+  background-color: transparent;
 
-	&.offset::before {
-		content: '';
-		height: 4rem;
-		display: block;
-		margin-top: -4rem;
-	}
+  &.offset::before {
+    content: "";
+    height: 4rem;
+    display: block;
+    margin-top: -4rem;
+  }
 `;
 const Section: React.FunctionComponent<ISectionProps> = (props) => {
-	return (
-		<StyledSection id={props.id} className={props.className}>
-			{props.children}
-		</StyledSection>
-	);
+  return (
+    <StyledSection id={props.id} className={props.className}>
+      {props.children}
+    </StyledSection>
+  );
 };
 
 export interface ISectionProps extends React.HtmlHTMLAttributes<any> {
-	id: string;
+  id?: string;
 }
 
 export default Section;
